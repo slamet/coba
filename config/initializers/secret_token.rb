b5da06e76d9d4a612f1043c3734bebca5f77b4db
slamet <slametnhd@gmail.com>
2012-11-29 22:02:07 -0800

    rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slamet::Application.config.secret_token = '9efc0d702f5bac413e24601991c284dc55afe4113dfb43f96035cd5fcbf90e088e85c16f4601a89413ee75f9b1670e54b91d748d5213328071a728809bd19f46'
